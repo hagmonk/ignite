c5a6d30f80988bc25b0775e78a315a22a07f6f3a
sboikov <sboikov@gridgain.com>
2017-06-09 13:25:51 +0300

    ignite-5272 tx

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteOnePhaseCommitNearReadersTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteOnePhaseCommitNearReadersTest.java
@@ -34,6 +34,7 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.transactions.Transaction;
 import org.apache.ignite.transactions.TransactionConcurrency;
 import org.apache.ignite.transactions.TransactionIsolation;
+import org.apache.ignite.transactions.TransactionRollbackException;
 
 import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
 import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
@@ -145,6 +146,21 @@ public class IgniteOnePhaseCommitNearReadersTest extends GridCommonAbstractTest 
      * @throws Exception If failed.
      */
     public void testPutReaderUpdatePrimaryFails1() throws Exception {
+        putReaderUpdatePrimaryFails(1);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testPutReaderUpdatePrimaryFails2() throws Exception {
+        putReaderUpdatePrimaryFails(0);
+    }
+
+    /**
+     * @param backups Backups number.
+     * @throws Exception If failed.
+     */
+    private void putReaderUpdatePrimaryFails(int backups) throws Exception {
         testSpi = true;
 
         final int SRVS = 3;
@@ -157,18 +173,20 @@ public class IgniteOnePhaseCommitNearReadersTest extends GridCommonAbstractTest 
 
         Ignite srv = ignite(0);
 
-        srv.createCache(cacheConfiguration(1));
+        srv.createCache(cacheConfiguration(backups));
 
         Ignite client1 = startGrid(SRVS);
 
         IgniteCache<Object, Object> cache1 = client1.createNearCache(DEFAULT_CACHE_NAME,
             new NearCacheConfiguration<>());
 
-        Integer key = primaryKey(srv.cache(DEFAULT_CACHE_NAME));
-
         Ignite client2 = startGrid(SRVS + 1);
 
+        client= false;
+
         IgniteCache<Object, Object> cache2 = client2.cache(DEFAULT_CACHE_NAME);
+
+        Integer key = primaryKey(srv.cache(DEFAULT_CACHE_NAME));
 
         cache1.put(key, 1);
 
@@ -176,7 +194,7 @@ public class IgniteOnePhaseCommitNearReadersTest extends GridCommonAbstractTest 
 
         IgniteFuture<?> fut = cache2.putAsync(key, 2);
 
-        U.sleep(2000);
+        U.sleep(1000);
 
         assertFalse(fut.isDone());
 
@@ -184,7 +202,56 @@ public class IgniteOnePhaseCommitNearReadersTest extends GridCommonAbstractTest 
 
         fut.get();
 
-        checkCacheData(F.asMap(key, 2), DEFAULT_CACHE_NAME);
+        checkCacheData(F.asMap(key, backups == 0 ? null : 2), DEFAULT_CACHE_NAME);
+
+        for (TransactionConcurrency concurrency : TransactionConcurrency.values()) {
+            for (TransactionIsolation isolation : TransactionIsolation.values()) {
+                srv = startGrid(0);
+
+                awaitPartitionMapExchange();
+
+                key = primaryKey(srv.cache(DEFAULT_CACHE_NAME));
+
+                cache1.put(key, 1);
+
+                spi(srv).blockMessages(GridNearTxPrepareResponse.class, client2.name());
+
+                try (Transaction tx = client2.transactions().txStart(concurrency, isolation)) {
+                    cache2.putAsync(key, 2);
+
+                    fut = tx.commitAsync();
+
+                    U.sleep(1000);
+
+                    assertFalse(fut.isDone());
+
+                    stopGrid(0);
+
+                    if (backups == 0)
+                        fut.get();
+                    else {
+                        try {
+                            fut.get();
+
+                            fail();
+                        }
+                        catch (TransactionRollbackException ignore) {
+                            // Expected.
+                        }
+                    }
+                }
+
+                checkCacheData(F.asMap(key, backups == 0 ? null : 1), DEFAULT_CACHE_NAME);
+
+                try (Transaction tx = client2.transactions().txStart(concurrency, isolation)) {
+                    cache2.putAsync(key, 2);
+
+                    tx.commit();
+                }
+
+                checkCacheData(F.asMap(key, 2), DEFAULT_CACHE_NAME);
+            }
+        }
     }
 
     /**
